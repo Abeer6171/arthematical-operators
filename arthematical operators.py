bee148e85ad5985696e7712154c1f410fc72a001
Abeer6171 <champchess984@gmail.com>
2025-01-24 15:17:07 +0530

    Add files via upload

---
+++ arthematical operators.py
@@ -0,0 +1,18 @@
+print("Welcome to my project")
+print("This is Addition 23+32")
+a = 23
+b = 32
+print(a + b)
+print("This is Subsctraction 23-32")
+c = 23
+d = 32
+print(c - b)
+print("This is Multiplication 23*32")
+e = 23
+f = 32
+print(e*f)
+print("This is Division of 23/32")
+g = 23
+h = 32
+print(g/h)
+print("Please vote my project")
